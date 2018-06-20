987d24c2d320974c7031226db399d12fe2ceb3a3
MininMobile <notminin@gmail.com>
2018-06-20 11:57:51 +0200

    Add RunFile Method

--- FlexScript/Program.cs
+++ FlexScript/Program.cs
@@ -10,23 +10,33 @@ namespace FlexScript {
         static void Main(string[] args) {
             if (args.Length == 0) {
                 Console.WriteLine("Launched FlexScript Console");
-            } else {
-                if (args[0] == "-f" || args[0] == "--file") {
-                    if (args.Length > 1) {
-                        if (File.Exists(args[1])) {
-                            Console.WriteLine(File.ReadAllText(args[1]));
-                        } else {
-                            throw new Exception("Invalid Arguments; invalid file specified");
-                        }
+            } else if (args[0] == "-f" || args[0] == "--file") {
+                if (args.Length > 1) {
+                    if (File.Exists(args[1])) {
+                        Console.WriteLine(RunFile(args[0]));
                     } else {
-                        throw new Exception("Invalid Arguments; asked to run file without specifying file");
+                        throw new Exception("Invalid Arguments; invalid file specified");
                     }
                 } else {
-                    throw new Exception("Invalid Arguments");
+                    throw new Exception("Invalid Arguments; asked to run file without specifying file");
                 }
+            } else if (File.Exists(args[0])) {
+                Console.WriteLine(RunFile(args[0]));
+            } else {
+                throw new Exception("Invalid Arguments");
             }
 
             Console.ReadKey();
+        }
+
+        static private string RunFile(string filePath) {
+            IEnumerable<string> lines = File.ReadLines(filePath);
+
+            string output = "";
+
+            output = string.Join(", ", lines);
+
+            return output;
         }
     }
 }

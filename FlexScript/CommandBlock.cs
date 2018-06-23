25fc334de7ede176e5bc1e0b2bfb61a54a7d64a7
MininMobile <notminin@gmail.com>
2018-06-23 23:55:58 +0200

    add CommandBlock class

---
+++ FlexScript/CommandBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexScript {
+	public class CommandBlock {
+		List<string> list;
+		string[] array;
+		int length;
+
+		#region Constructors
+		public CommandBlock(string[] lines) {
+			length = lines.Length;
+			list = lines.ToList();
+			array = lines;
+		}
+
+		public CommandBlock(List<string> lines) {
+			length = lines.ToArray().Length;
+			array = lines.ToArray();
+			list = lines;
+		}
+		#endregion
+	}
+}
